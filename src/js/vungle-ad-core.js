bc0c21ca1c9d46657bdee142714bc5f6e4cad6fd
Daniele Delgrosso <daniele.delgrosso@vungle.com>
2018-02-07 14:55:55 +0000

    Added VungleAd to JSCore, theme colour added to body, updated NODE_ENV filters in HBS

--- src/js/vungle-ad-core.js
+++ src/js/vungle-ad-core.js
@@ -1,7 +1,11 @@
 /* ----- Vungle Design Framework - JS ad core initialisation ----- */
 
+var VungleAd = {};
+
 var closeRevealDelay = 4000;
 var closeRevealInteractionDelay = 1000;
+
+VungleAd.adSizes = ['xl', 'l', 'm', 's', 'xs'];
 
 var vungleAd = document.getElementById('vungle-ad');
 
@@ -13,22 +17,28 @@ document.ontouchmove = function(event) {
     event.preventDefault();
 }
 
-var vungleAdSizes = ['xl', 'l', 'm', 's', 'xs'];
+window.addEventListener('resize', function(event) {
+    vungleAd.style.opacity = 0;
+
+    if (this.resizeTimer) {
+        clearTimeout(this.resizeTimer);
+    }
+    this.resizeTimer = setTimeout(function() {
+        renderVungleAdSizingClass();
+        vungleAd.style.opacity = 1;
+    }, 200);
+});
 
 function initVungleAd() {
+    //Called when Ad loads
+    renderVungleAdSizingClass();
+}
+
+function renderVungleAdSizingClass() {
 
     var vungleAdContainer = document.getElementById('vungle-ad-container');
-
     var longestSide = (Math.max(vungleAdContainer.offsetHeight, vungleAdContainer.offsetWidth));
     var shortestSide = (Math.min(vungleAdContainer.offsetHeight, vungleAdContainer.offsetWidth));
-
-    /*
-        The original innerWidth/Height potentially breaks ad when viewed on the latest Chrome,
-        so we have gone ahead and used offsetWidth/Height within the body tag CSS
-        
-        var longestSide = (Math.max(window.innerHeight, window.innerWidth));
-        var shortestSide = (Math.min(window.innerHeight, window.innerWidth));
-    */
 
     if (typeof theme === 'undefined' || theme === null) {
         theme = 'dark';
@@ -40,50 +50,50 @@ function initVungleAd() {
     // @endif
 
     var range = ((longestSide - shortestSide) / longestSide) * 100;
-    var adClassName;
+    var adShape, computedSize;
+    var adSizeClass = '';
 
     if (longestSide === shortestSide) {
         //Square ad unit
-        adClassName = 'square';
+        adShape = 'square';
 
     } else if (window.innerHeight > window.innerWidth) {
         //Portrait ad unit
-        if (vungleAdSizes[(Math.ceil(range / 10) * 1) - 1]) {
-            adClassName = 'portrait portrait-' + vungleAdSizes[(Math.ceil(range / 10) * 1) - 1];
+        adShape = 'portrait';
+        if (VungleAd.adSizes[(Math.ceil(range / 10) * 1) - 1]) {
+            computedSize = VungleAd.adSizes[(Math.ceil(range / 10) * 1) - 1];
+            adSizeClass = ' portrait-' + computedSize;
         } else {
-            adClassName = 'portrait portrait-' + vungleAdSizes[vungleAdSizes.length - 1] + ' oob';
+            computedSize = VungleAd.adSizes[VungleAd.adSizes.length - 1];
+            adSizeClass = ' portrait-' + computedSize + ' oob';
         }
-
     } else {
         //Landscape ad unit
-        if (vungleAdSizes[(Math.ceil(range / 10) * 1) - 1]) {
-            adClassName = 'landscape landscape-' + vungleAdSizes[(Math.ceil(range / 10) * 1) - 1];
+        adShape = 'landscape';
+        if (VungleAd.adSizes[(Math.ceil(range / 10) * 1) - 1]) {
+            computedSize = VungleAd.adSizes[(Math.ceil(range / 10) * 1) - 1];
+            adSizeClass = ' landscape-' + computedSize;
         } else {
-            adClassName = 'landscape landscape-' + vungleAdSizes[vungleAdSizes.length - 1] + ' oob';
+            computedSize = VungleAd.adSizes[VungleAd.adSizes.length - 1];
+            adSizeClass = ' landscape-' + computedSize + ' oob';
         }
     }
+    adClassName = adShape.concat(adSizeClass);
 
     //Append body tag with appropriate classnames
     document.body.className = adClassName + ' ' + theme + ' ' + getOS();
 
-    window.addEventListener('resize', function(event) {
-        vungleAd.style.opacity = 0;
-
-        if (this.resizeTimer) {
-            clearTimeout(this.resizeTimer);
-        }
-        this.resizeTimer = setTimeout(function() {
-            initVungleAd();
-            vungleAd.style.opacity = 1;
-        }, 200);
-    });
+    VungleAd.shape = adShape;
+    VungleAd.sizeClass = computedSize;
+    VungleAd.theme = theme;
+    VungleAd.os = getOS();
 
     // @ifdef NODE_ENV='dev' 
     /*  
         Debug mode: Displays the Vungle boilerplate classes to help you
         identify each classname if you wish to make additional stylistic changes
     */
-    if (typeof debug !== 'undefined' || debug !== null) {
+    if (typeof debug !== 'undefined' && debug !== null) {
         if (debug) {
             var adClassDebug;
             if (adClassName.indexOf('oob') >= 0) {
@@ -108,23 +118,26 @@ function initVungleAd() {
     }
     // @endif
 
-    /* close button timer implementation */
+    /* Close Button Timer */
+    var closeButton = document.getElementById('vungle-close');
+    if (typeof closeButtonTimer !== 'undefined' && closeButtonTimer !== null) {
+        switch (closeButtonTimer) {
+            case 'NOTIMER':
+                revealCloseButton();
+                break;
 
-    var close = document.getElementById('vungle-close');
+            case 'TIMER':
+                setTimeout(revealCloseButton, closeRevealDelay);
+                document.addEventListener('click', function() {
+                    setTimeout(revealCloseButton, closeRevealInteractionDelay);
+                });
+                break;
 
-    switch (VUNGLE_TIMER) {
-        case 'NOTIMER':
-
-            revealCloseButton();
-            break;
-        case 'TIMER':
-
-            setTimeout(revealCloseButton, closeRevealDelay);
-
-            document.addEventListener('click', function() {
-                setTimeout(revealCloseButton, closeRevealInteractionDelay);
-            });
-            break;
+            default:
+                revealCloseButton();
+        }
+    } else {
+        revealCloseButton();
     }
 }
 
