ffe6fbcf79f11338b6138d6ffddadf48051e926a
patrickdrabble <patrick.drabble@vungle.com>
2019-02-21 10:59:55 +0000

    seeing if async code works

--- gulp-scripts/commit-validation.js
+++ gulp-scripts/commit-validation.js
@@ -1,6 +1,8 @@
 var fs = require('fs-extra');
-     const commitMessage = fs.readFile('./git/COMMIT_EDITMSG');
-     console.log(commitMessage)
-     console.error('works');
-     fail();
 
+(async () => {
+    const commitMessage = await fs.readFile('./git/COMMIT_EDITMSG');
+    console.log(commitMessage)
+    console.error('works');
+    fail();
+})();
