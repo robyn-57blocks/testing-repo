bcfadf9dfeef890ddede1fde69142634a7185c00
Marian Barrilao <marian.barrilao@vungle.com>
2019-09-13 18:40:26 +0100

    VCI-538 Reading external DS file and appending it to core styles

--- gulp-scripts/build-sass.js
+++ gulp-scripts/build-sass.js
@@ -6,18 +6,30 @@ const livereload = require('gulp-livereload');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const replace = require('gulp-replace');
+const request = require('request');
+const through = require('through2');
+const fs = require('fs');
 
 // gulp scripts
 const isEnvDev = require('./env.js').isEnvDev;
 const config = require('./config.js');
 
-module.exports = function() {
+const external = 'https://vungle-design-system.s3.eu-west-1.amazonaws.com/creative.min.css';
 
+module.exports = function() {
     return gulp.src(config.src + '/scss/**/*.scss')
         .pipe(gulpif(isEnvDev(), concat('styles.scss'), concat('vungle.min.scss')))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulpif(!isEnvDev(), replace('img/', '')))
         .pipe(gulpif(!isEnvDev(), cleanCSS()))
         .pipe(gulpif(isEnvDev(), gulp.dest(config.dist + '/css'), gulp.dest(config.dist)))
+        .pipe(gulpif(!isEnvDev(), through.obj(function (chunk, enc, cb) {
+          request(external, (error, response, body) => {
+            fs.appendFile('./dist/vungle.min.css', response.body.toString(), 'utf8', (err) => {
+              if (err) throw err;
+              cb(null, chunk);
+            });
+          });
+        })))
         .pipe(livereload());
 }
