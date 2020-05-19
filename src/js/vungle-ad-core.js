8a191f057c1181f1f5a1aad2329ab6ffc5756d29
Daniele Delgrosso <daniele.delgrosso@vungle.com>
2020-05-19 17:45:49 +0100

    Removed any reference to vw/vh, which was only used for debug mode. Debug mode and vungle ad sizing classes were deprecated because of Design System, so cleared references to them as well.

--- src/js/vungle-ad-core.js
+++ src/js/vungle-ad-core.js
@@ -89,9 +89,6 @@ var adcore = {
             event.preventDefault();
         };
 
-        //Called when Ad loads
-        renderVungleAdSizingClass();
-
         function getDynamicElement() {
             if (!dynamicElement) {
                 dynamicElement = document.querySelector("#dynamic");
@@ -377,100 +374,6 @@ var adcore = {
         function endcardOnlyVideoAttribution() {
             //Used to ensure endcard only (short-form) creatives are served successfully
             EndcardOnlyAttribution.startTimer();
-        }
-
-        function renderVungleAdSizingClass() {
-
-            var vungleAdContainer = document.getElementById('dynamic');
-            var longestSide = (Math.max(vungleAdContainer.offsetHeight, vungleAdContainer.offsetWidth));
-            var shortestSide = (Math.min(vungleAdContainer.offsetHeight, vungleAdContainer.offsetWidth));
-
-            var theme;
-            if (typeof theme === 'undefined' || theme === null) {
-                theme = 'light';
-            }
-
-            var minimumContainerSize = (minimumPercentageContainerSize / 100 * longestSide);
-            var adSizeSegmentLength = (longestSide - minimumContainerSize) / VungleAd.adSizes.length;
-            var arrayCount = Math.floor((shortestSide - minimumContainerSize) / adSizeSegmentLength);
-
-            var adShape, computedSize;
-            var adSizeClass = '';
-
-            if (longestSide === shortestSide) {
-                //Square ad unit
-                adShape = 'square';
-
-            } else if (document.body.clientHeight > document.body.clientWidth) {
-                //Portrait ad unit
-                adShape = 'portrait';
-                if (VungleAd.adSizes[arrayCount]) {
-                    computedSize = VungleAd.adSizes[arrayCount];
-                    adSizeClass = ' portrait-' + computedSize;
-                } else {
-                    computedSize = VungleAd.adSizes[VungleAd.adSizes.length - 1];
-                    adSizeClass = ' portrait-' + computedSize + ' oob';
-                }
-            } else {
-                //Landscape ad unit
-                adShape = 'landscape';
-                if (VungleAd.adSizes[arrayCount]) {
-                    computedSize = VungleAd.adSizes[arrayCount];
-                    adSizeClass = ' landscape-' + computedSize;
-                } else {
-                    computedSize = VungleAd.adSizes[arrayCount];
-                    adSizeClass = ' landscape-' + computedSize + ' oob';
-                }
-            }
-
-            var adClassName = adShape.concat(adSizeClass);
-
-            //Append body tag with appropriate classnames
-            document.body.className = adClassName + ' ' + theme + ' ' + AdHelper.getOS();
-
-            VungleAd.shape = adShape;
-            VungleAd.sizeClass = computedSize;
-            VungleAd.theme = theme;
-            VungleAd.os = AdHelper.getOS();
-
-            // @if NODE_ENV='dev'
-            /*
-                Debug mode: Displays the Vungle boilerplate classes to help you
-                identify each classname if you wish to make additional stylistic changes
-            */
-
-            if (window.vungleDebugMode === true) {
-                var adClassDebug;
-                if (adClassName.indexOf('oob') >= 0) {
-                    adClassDebug = '<p><span class="title">Out of Bounds</span>\
-                                <span>Pixel Density: <b>' + window.devicePixelRatio + '</b></span>\
-                                <span>Longest Side: <b>' + longestSide + 'px</b></span>\
-                                <span>Shortest Side: <b>' + shortestSide + 'px</b></span>\
-                                <span>Minimum Container Size: <b>' + minimumContainerSize + 'px</b></span>\
-                                </p>';
-                } else {
-                    adClassDebug = '<p><span class="title">' + adClassName + '</span>\
-                                <span>Pixel Density: <b>' + window.devicePixelRatio + '</b></span>\
-                                <span>Longest Side: <b>' + longestSide + 'px</b></span>\
-                                <span>Shortest Side: <b>' + shortestSide + 'px</b></span>\
-                                <span>Minimum Container Size: <b>' + minimumContainerSize + 'px</b></span>\
-                                </p>';
-                }
-
-                if (document.getElementById("vungle-ad-debug") === null) {
-                    var debugElem = document.createElement("div");
-                    debugElem.setAttribute("id", "vungle-ad-debug");
-                    debugElem.className = adClassName;
-                    debugElem.innerHTML = adClassDebug;
-
-                    document.body.appendChild(debugElem);
-                } else {
-                    var debugContainer = document.getElementById("vungle-ad-debug");
-                    debugContainer.className = adClassName;
-                    debugContainer.innerHTML = adClassDebug;
-                }
-            }
-            // @endif
         }
 
         function revealAdNotificationModal() {
